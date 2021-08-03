901a9dc4601356d608c14437de1aeeeeacc99fc7
dynami07 <88266746+dynami07@users.noreply.github.com>
2021-08-03 13:31:24 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ToDoItem from "./ToDoItem";
 import InputArea from "./InputArea";
 import date from "./date";
+import'./styles.css';
+import './switcher.scss';
 
 function App() {
+  const [ colorTheme, setColorTheme]= useState('theme-white');
+
+useEffect(() => {
+	const currentThemeColor = localStorage.getItem('theme-color');
+	if(currentThemeColor)
+	{
+	   setColorTheme(currentThemeColor);
+	}
+}, []);
+
+const handleClick= (theme) => {
+	setColorTheme(theme);
+	localStorage.setItem('theme-color',theme)
+  
   const [items, setItems] = useState([]);
 
   function addItem(inputText) {
     setItems(prevItems => {
       return [...prevItems, inputText];
+      
     });
   }
 
@@ -38,6 +55,46 @@ function App() {
           ))}
         </ul>
       </div>
+    
+      <div className={`App ${colorTheme}`">
+	<div className= 'theme-options'>
+
+	  <div id ='theme-white'
+		onClick={() => handleClick('theme-white')}
+		className={`${colorTheme ==== 'theme-white' ? 'active' : ''}`}	  
+	  />
+	  <div id ='theme-blue'
+		onClick={() => handleClick('theme-blue')}
+	 	className={`${colorTheme ==== 'theme-blue' ? 'active' : ''}`}
+	  />
+          <div id ='theme-orange'
+		onClick={() => handleClick('theme-orange')}
+		className={`${colorTheme ==== 'theme-orange' ? 'active' : ''}`}
+	  />
+          <div id ='theme-purple'
+		onClick={() => handleClick('theme-purple')}
+		className={`${colorTheme ==== 'theme-purple' ? 'active' : ''}`}
+	  />
+	  <div id ='theme-green'
+		onClick={() => handleClick('theme-green')}
+		className={`${colorTheme ==== 'theme-green' ? 'active' : ''}`}
+	  />
+	  <div id ='theme-black'
+		onClick={() => handleClick('theme-black')}
+		className={`${colorTheme ==== 'theme-black' ? 'active' : ''}`}
+	  />
+	</div>
+
+	<div className= 'content-box'>
+	<h3 Multiple Theme Switcher>
+	<p>
+	djh,fnhjvbfd,vsvhfshskhskggfkuhfyur
+	</p>
+	</div>
+</div>
+
+
+
     </div>
   );
 }
