080f2d2cc35006b35b81ddaec98bc59ab7b08d5c
dynami07 <88266746+dynami07@users.noreply.github.com>
2021-08-03 14:56:09 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ToDoItem from "./ToDoItem";
 import InputArea from "./InputArea";
 import './switcher.scss';
