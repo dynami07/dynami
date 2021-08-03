2dcc83ce3e23dee053c038706c097a2c04d89895
dynami07 <88266746+dynami07@users.noreply.github.com>
2021-08-03 14:24:55 +0530

    Create App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function addItem(inputText) {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+  }  
+  
+  const [ colorTheme, setColorTheme]= useState('theme-white');
+
+  useEffect(() => {
+	const currentThemeColor = localStorage.getItem('theme-color');
+	if(currentThemeColor)
+	{
+	   setColorTheme(currentThemeColor);
+	}
+}, []);
+
+  const handleClick= (theme) => {
+	setColorTheme(theme);
+	localStorage.setItem('theme-color',theme)
+  
+ 
+
+  function deleteItem(id) {
+    setItems(prevItems => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea addItem={addItem} />
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+<div className={`App ${colorTheme}`">
+	<div className= 'theme-options'>
+
+	  <div id ='theme-white'
+		onClick={() => handleClick('theme-white')}
+		className={`${colorTheme ==== 'theme-white' ? 'active' : ''}`}	  
+	  />
+	  <div id ='theme-blue'
+		onClick={() => handleClick('theme-blue')}
+	 	className={`${colorTheme ==== 'theme-blue' ? 'active' : ''}`}
+	  />
+          <div id ='theme-orange'
+		onClick={() => handleClick('theme-orange')}
+		className={`${colorTheme ==== 'theme-orange' ? 'active' : ''}`}
+	  />
+          <div id ='theme-purple'
+		onClick={() => handleClick('theme-purple')}
+		className={`${colorTheme ==== 'theme-purple' ? 'active' : ''}`}
+	  />
+	  <div id ='theme-green'
+		onClick={() => handleClick('theme-green')}
+		className={`${colorTheme ==== 'theme-green' ? 'active' : ''}`}
+	  />
+	  <div id ='theme-black'
+		onClick={() => handleClick('theme-black')}
+		className={`${colorTheme ==== 'theme-black' ? 'active' : ''}`}
+	  />
+	</div>
+
+	<div className= 'content-box'>
+	<h3 Multiple Theme Switcher>
+	<p>
+	djh,fnhjvbfd,vsvhfshskhskggfkuhfyur
+	</p>
+	</div>
+</div>
+  );
+}
+
+export default App;
