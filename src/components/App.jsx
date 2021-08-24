77d13f28c651884e920e23c58ad49c4b93bb99fd
HIMANSHU <88266746+himanshu070999@users.noreply.github.com>
2021-08-24 15:56:11 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -40,6 +40,24 @@ setItems(prevItems => {
 let gender=["Male","Female"];
 const [displayGender,setGender]=useState();
 	
+	class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+	
   return (
 	  
 <div className={`App ${colorTheme}`}>
@@ -89,7 +107,18 @@ const [displayGender,setGender]=useState();
 			</center>
 	</div>
 	
-	   
+	<form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>   
       
       <div>
       
