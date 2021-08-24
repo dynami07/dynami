c496023acb291d05601ef5bd37fd46d01f349bb8
HIMANSHU <88266746+himanshu070999@users.noreply.github.com>
2021-08-24 16:05:27 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -40,24 +40,8 @@ setItems(prevItems => {
 let gender=["Male","Female"];
 const [displayGender,setGender]=useState();
 	
-	class FlavorForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: 'coconut'};
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('Your favorite flavor is: ' + this.state.value);
-    event.preventDefault();
-  }
-	}	
+		
   return (
 	  
 	<div className={`App ${colorTheme}`}>
