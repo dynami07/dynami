201bc34029559bfdb19ec275f89f9bd44ef05c1f
HIMANSHU <88266746+himanshu070999@users.noreply.github.com>
2021-08-24 16:07:33 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -91,10 +91,10 @@ const [displayGender,setGender]=useState();
 			</center>
 	</div>
 	
-	<form onSubmit={this.handleSubmit}>
+	<form>
         <label>
           Pick your favorite flavor:
-          <select value={this.state.value} onChange={this.handleChange}>
+          <select>
             <option value="grapefruit">Grapefruit</option>
             <option value="lime">Lime</option>
             <option value="coconut">Coconut</option>
