f78e301ea67f60a16e30deeaacb546cebbad37e4
HIMANSHU <88266746+himanshu070999@users.noreply.github.com>
2021-08-25 08:15:10 +0530

    Create InputGender.jsx

---
+++ src/components/InputGender.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function InputGender(props) {
+  const [inputgender, setInputGender] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputGender(newValue);
+  }
+  let gender=["Male","Female"];
+  
+  return (
+	<div>
+		<center>
+		{gender.map(result=>(
+			<>
+			<input type="radio" value={result} name="radiovalues"  onChange={handleChange} />
+			<b> {result} </b>
+			</>
+			))}
+			</center>
+	</div>
+
+  );
+}
+
+export default InputGender;
