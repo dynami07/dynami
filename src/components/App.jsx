faef6c6096cb53699acbf95a091c278a9e436d56
HIMANSHU <88266746+himanshu070999@users.noreply.github.com>
2021-08-26 13:58:02 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,6 +37,12 @@ setItems(prevItems => {
     });
   }
 	
+	
+	let gender=["Male","Female"];
+const [displayGender,setGender]=useState();
+	
+const [displayGenderr,setGenderr]=useState();
+
 
 
 
@@ -79,6 +85,34 @@ setItems(prevItems => {
       <InputArea addItem={addItem} />
     
              
+	     
+	     
+	     <div>
+		<center>
+		{gender.map(result=>(
+			<>
+			<input type="radio" value={result} name="radiovalues" onChange={(e)=>setGender(e.target.value)}/>
+			<b> {result} </b>
+			</>
+			))}
+			</center>
+	</div>
+	
+	<form>
+        <label>
+          Gender: &nbsp;	  
+          <select  onChange={(e)=>setGenderr(e.target.value)}>
+	    <option value="Select Gender">Select</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+        </label>
+   
+      </form>   
+      
+      
+      
+	     
       <div>
          <ul>
           {items.map((todoItem, index) => (
