bed33adb10bcf9f495f99eff998eb9775c186e3d
HIMANSHU <88266746+himanshu070999@users.noreply.github.com>
2021-08-27 09:24:08 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -86,7 +86,7 @@ const [displayGenderr,setGenderr]=useState();
 		<center>
 		{gender.map(result=>(
 			<>
-			<input type="radio" value={result} name="radiovalues" onChange={(e)=>setGendere.target.value)}/>
+			<input type="radio" value={result} name="radiovalues" onChange={(e)=>setGender(e.target.value)}/>
 			<b> {result} </b>
 			</>
 			))}
