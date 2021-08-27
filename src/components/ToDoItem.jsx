cdd30a84a2f6551ba46d3d0f65f35f8f45619978
HIMANSHU <88266746+himanshu070999@users.noreply.github.com>
2021-08-27 09:07:30 +0530

    Update ToDoItem.jsx

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -9,7 +9,8 @@ function ToDoItem(props) {
     >
       <li>
         {props.text}
-               
+               <bold className="display"> {props.display}  {props.show} </bold>
+        
        </li>
     </div>
   );
